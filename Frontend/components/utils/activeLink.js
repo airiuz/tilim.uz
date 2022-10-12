edca74c9d341bcdc5fb6713d6e9aa35685093c16
ulugbek <iamucmonov@gmail.com>
2022-10-12 17:50:19 +0500

    layout

--- Frontend/components/utils/activeLink.js
+++ Frontend/components/utils/activeLink.js
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-function ActiveLink({ href, children, active, icon }) {
+function ActiveLink({ href, children, active }) {
   const { asPath } = useRouter();
 
   return (
     <Link href={href}>
       <a
-        className={`px-3 flex items-center gap-[2px] ${
-          asPath === href && active
+        className={`flex items-center text-lg ${
+          asPath === href ? active : "text-[#273A5D] "
         }`}
       >
-        {icon} {children}
+        {children}
       </a>
     </Link>
   );

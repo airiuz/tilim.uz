7ffd278e7f7fba09d8b24e8826cd2943ddda82f8
shakhriyorm <shakhriyormamadaliev@gmail.com>
2022-10-18 15:09:27 +0500

    fix

--- Frontend/pages/contact.js
+++ Frontend/pages/contact.js
@@ -14,53 +14,51 @@ function Contact() {
   return (
     <>
       <Layout>
-        <section className="bg-[#ECECEC]  flex-1 relative z-10">
-          <div className="flex items-center justify-center absolute inset-0">
-            <div className="w-[800px] h-[458px] bg-white flex items-center  justify-between   border pl-[60px] pr-[69px] rounded-xl">
-              <div>
-                <div className="space-y-[35px]">
-                  <p className="text-[#273A5D] text-[24px] font-semibold">
-                    Biz bilan bog’lanish
-                  </p>
-                  <Link href={"tel:+998 9000 96009"}>
-                    <a className="flex items-center space-x-[21px]">
-                      {phone}
-                      <span>+998 9000 96009</span>
-                    </a>
-                  </Link>
-                  <Link href={"email:info@airi.com"}>
-                    <a className="flex items-center space-x-[21px]">
-                      {email}
-                      <span>info@airi.com</span>
-                    </a>
-                  </Link>
-                </div>
-                <div className="mt-[60px] space-y-[40px]">
-                  <p className="text-[#273A5D] text-[24px] font-semibold">
-                    Ijtimoiy tarmoqlarda kuzating
-                  </p>
-                  <div className="flex space-x-[66px] pl-6">
-                    <Link href={"#"}>
-                      <a>{facebook}</a>
-                    </Link>
-                    <Link href={"#"}>
-                      <a>{instagram}</a>
-                    </Link>
-                    <Link href={"#"}>
-                      <a>{telegram}</a>
-                    </Link>
-                    <Link href={"#"}>
-                      <a>{github}</a>
-                    </Link>
-                  </div>
-                </div>
+        <div className="bg-[#F6F6F7] grid place-content-center py-[100px]">
+          <div className="w-[800px] h-[458px] bg-white flex items-center justify-between pl-[60px] pr-[69px] rounded-xl">
+            <div>
+              <div className="space-y-[35px]">
+                <p className="text-[#273A5D] text-[24px] font-semibold">
+                  Biz bilan bog’lanish
+                </p>
+                <Link href={"tel:+998 9000 96009"}>
+                  <a className="flex items-center space-x-[21px]">
+                    {phone}
+                    <span>+998 9000 96009</span>
+                  </a>
+                </Link>
+                <Link href={"email:info@airi.com"}>
+                  <a className="flex items-center space-x-[21px]">
+                    {email}
+                    <span>info@airi.com</span>
+                  </a>
+                </Link>
               </div>
-              <div>
-                <img src="/img/logo.png" alt="logo" />
+              <div className="mt-[60px] space-y-[40px]">
+                <p className="text-[#273A5D] text-[24px] font-semibold">
+                  Ijtimoiy tarmoqlarda kuzating
+                </p>
+                <div className="flex space-x-8">
+                  <Link href={"#"}>
+                    <a>{facebook}</a>
+                  </Link>
+                  <Link href={"#"}>
+                    <a>{instagram}</a>
+                  </Link>
+                  <Link href={"#"}>
+                    <a>{telegram}</a>
+                  </Link>
+                  <Link href={"#"}>
+                    <a>{github}</a>
+                  </Link>
+                </div>
               </div>
             </div>
+            <div>
+              <img src="/img/logo.png" alt="logo" />
+            </div>
           </div>
-        </section>
+        </div>
       </Layout>
     </>
   );
--- Frontend/pages/security.js
+++ Frontend/pages/security.js
@@ -4,118 +4,126 @@ function Security() {
   return (
     <>
       <Layout>
-        <section className="bg-[#F6F6F7] max-w-[1200px] mx-auto pl-[100px] pr-[170px] py-[50px] my-[30px]">
-          <p className="text-[36px] font-semibold">Xavfsizlik siyosati</p>
-          <p className="text-base my-[17px]">
-            “Xavfsizlik siyosati” hujjati (bundan buyon matnda “siyosat” deb
-            yuritiladi) Internet foydalanuvchilari (bundan keyin – “siz” yoki
-            “maʼlumotlar”dan foydalanish qoidalarini anglatadi) va sayt
-            egasining (bundan keyin – “biz” yoki “Maʼmuriyat”) maʼlumotlarini
-            anglatadi.
-          </p>
-          <div className="space-y-[15px]">
-            <p>
-              <span className="font-semibold text-lg">
-                1. Qayta ishlangan maʼlumotlar
-              </span>
-              <br /> 1.1. Saytdagi imkoniyatlardan foydalangan holda biz sizning
-              shaxsiy maʼlumotlaringizni yig‘maymiz. <br /> 1.2. Saytda
-              to‘plangan barcha maʼlumotlar (bundan keyin “Anonimlashtirilgan
-              maʼlumotlar” deb nomlanadi) anonim shaklda qabul qilinadi.
-              <br /> 1.3. Anonim maʼlumotlar quyidagilarni o‘z ichiga oladi:
-              <br />
-              1.3.1. Saytning onlayn imkoniyatlari va dasturiy modullaridan
-              foydalanib, foydalanuvchining ismi, telefon raqami va elektron
-              pochta manzilidan foydalangan holdagi maʼlumot;
-              <br /> 1.3.2. Siz foydalanadigan dasturiy taʼminot sozlamalariga
-              qarab, avtomatik ravishda anonim shaklda uzatiladigan maʼlumotlar.
-              <br />
-              1.4. Maʼmuriyat foydalanuvchidan Sayt yordamida to‘plangan anonim
-              maʼlumotlarning tarkibidagi talablarni belgilash huquqiga ega.
-              <br />
-              1.5. Agar biror-bir maʼlumot majburiy deb belgilanmagan bo‘lsa,
-              uni taqdim etish yoki oshkor qilish Foydalanuvchi ixtiyorida
-              bo‘ladi. Shu bilan birga, siz cheksiz doiralarning bunday
-              maʼlumotlarga kirishiga rozilik bildirasiz. Ko‘rsatilgan
-              maʼlumotlar taqdim qilingan yoki boshqa shaklda ochilgan paytdan
-              boshlab hammaga ochiq bo‘ladi.
-              <br /> 1.6. Maʼmuriyat taqdim etilgan maʼlumotlarning
-              to‘g‘riligini tekshirmaydi va Foydalanuvchi ushbu Siyosatga
-              muvofiq ularni qayta ishlashga zarur roziligini oladi.
-              Foydalanuvchi vijdonan harakat qiladi va ehtiyotkorlik bilan
-              ishlaydi hamda bunday maʼlumotlarni yangilab turish, undan
-              foydalanish uchun barcha zarur roziliklarni olishga harakat
-              qiladi. Siz Saytda uchinchi tomon dasturlaridan foydalanish
-              imkoniyatini tan olasiz va qabul qilasiz, buning natijasida bunday
-              shaxslar 1.3-bandda ko‘rsatilgan maʼlumotlarni sizga nomaʼlum
-              shaklda olishlari va uzatishlari mumkin.
-              <br /> 1.7. Uchinchi tomon dasturlari yordamida anonim maʼlumotni
-              yig‘ish tarkibi va shartlari bevosita ularning egalari tomonidan
-              belgilanadi hamda quyidagilarni o‘z ichiga olishi mumkin: -
-              Brauzer maʼlumotlari (turi, versiyasi, “cookie” fayllari); -
-              Qurilma maʼlumotlari va uning holati; - Operatsion tizim
-              maʼlumotlari (turi, versiyasi, ekran o‘lchamlari); - So‘rov
-              maʼlumotlari (vaqt, o‘tish manbai, IP manzili).
-              <br /> 1.8. Maʼmuriyat uchinchi tomon tomonidan foydalanuvchining
-              Anonim maʼlumotlarini ishlatish tartibi uchun javobgar bo‘lmaydi.
+        <div className="bg-[#F6F6F7]">
+          <div className="bg-white rounded-xl max-w-[1000px] mx-auto px-[100px] py-[50px] mb-[100px] mt-[40px]">
+            <p className="text-[36px] font-semibold">Xavfsizlik siyosati</p>
+            <p className="text-base my-[17px]">
+              “Xavfsizlik siyosati” hujjati (bundan buyon matnda “siyosat” deb
+              yuritiladi) Internet foydalanuvchilari (bundan keyin – “siz” yoki
+              “maʼlumotlar”dan foydalanish qoidalarini anglatadi) va sayt
+              egasining (bundan keyin – “biz” yoki “Maʼmuriyat”) maʼlumotlarini
+              anglatadi.
             </p>
-            <p>
-              <span className="font-semibold text-lg">
-                2. Maʼlumotlarni qayta ishlash maqsadlari
-              </span>{" "}
-              <br /> 2.1. Maʼmuriyat maʼlumotlardan quyidagi maqsadlarda
-              foydalanadi: <br />
-              2.1.1. Kirish so‘rovlari va foydalanuvchi bilan aloqani qayta
-              ishlash;
-              <br /> 2.1.2. Axborot xizmatlari, shu jumladan reklama
-              materiallarini tarqatish;
-              <br /> 2.1.3. Marketing, statistik va boshqa tadqiqotlarini
-              o‘tkazish;
-              <br /> 2.1.4. Saytdagi reklama materiallarini yo‘naltirish.
-            </p>
-            <p>
-              <span className="font-semibold text-lg">
-                3. Maʼlumotlarni himoya qilish talablari
-              </span>{" "}
-              <br />
-              3.1. Maʼmuriyat maʼlumotlarni saqlaydi va ularni ichki qoidalarga
-              muvofiq ruxsatsiz kirish va tarqatishdan himoya qiladi. <br />{" "}
-              3.2. Olingan maʼlumotlarga nisbatan maxfiylik saqlanadi, agar ular
-              Foydalanuvchi tomonidan ochiq bo‘lsa, shuningdek saytda
-              ishlatiladigan uchinchi tomon texnologiyalari va dasturlari yoki
-              foydalanuvchi tomonidan ishlatiladigan dasturiy taʼminot ushbu
-              shaxslar yoki boshqa foydalanuvchilar va internet
-              foydalanuvchilari bilan ochiq almanishuvni taʼminlagan hollar
-              bundan mustasno. <br /> 3.3. Ish sifatini yaxshilash uchun 1 (bir)
-              yil davomida Saytdan foydalanish doirasida Foydalanuvchi tomonidan
-              amalga oshirilgan harakatlar to‘g‘risidagi jurnal fayllarini
-              saqlash huquqiga ega.
-            </p>
-            <p>
-              <span className="font-semibold text-lg">4. Maʼlumot uzatish</span>
-              <br /> 4.1. Maʼmuriyat quyidagi hollarda maʼlumotlarni uchinchi
-              shaxslarga uzatish huquqiga ega: - Foydalanuvchi bunday
-              harakatlarga, shu jumladan foydalanuvchi maʼlum bir maʼlumotni
-              berishni cheklamaydigan dasuturiy taʼminot sozlamalarini ishlatgan
-              holatlarda roziligini bildiradi; - Maʼlumotni qayta ishlash,
-              maqsadga muvofiq uzatish talab etiladi: - Foydalanuvchi tomonidan
-              sodir etilgan qonunbuzarlik sababli, Maʼmuriyat huquqlari qonuniy
-              himoya qilinadi.
-            </p>
-            <p>
-              <span className="font-semibold text-lg">
-                5. Maxfiylik siyosatining o‘zgarishi
-              </span>
-              <br />
-              5.1. Ushbu Siyosat Maʼmuriyat tomonidan bir tomonlama tartibda
-              foydalanuvchi tomonidan ogohlantirilmasdan o‘zgartilirishi yoki
-              bekor qilinishi mumkin. Agar Siyosatning yangi variantida
-              boshqacha qoidalar nazarda tutilgan bo‘lmasa, yangi siyosat Saytga
-              joylashtirilgan paytdan boshlab kuchga kiradi. <br /> 5.2.
-              Siyosatning joriy talqini saytga joylashtirilgan.
-            </p>
+            <div className="space-y-[15px]">
+              <p>
+                <span className="font-semibold text-lg">
+                  1. Qayta ishlangan maʼlumotlar
+                </span>
+                <br /> 1.1. Saytdagi imkoniyatlardan foydalangan holda biz
+                sizning shaxsiy maʼlumotlaringizni yig‘maymiz. <br /> 1.2.
+                Saytda to‘plangan barcha maʼlumotlar (bundan keyin
+                “Anonimlashtirilgan maʼlumotlar” deb nomlanadi) anonim shaklda
+                qabul qilinadi.
+                <br /> 1.3. Anonim maʼlumotlar quyidagilarni o‘z ichiga oladi:
+                <br />
+                1.3.1. Saytning onlayn imkoniyatlari va dasturiy modullaridan
+                foydalanib, foydalanuvchining ismi, telefon raqami va elektron
+                pochta manzilidan foydalangan holdagi maʼlumot;
+                <br /> 1.3.2. Siz foydalanadigan dasturiy taʼminot sozlamalariga
+                qarab, avtomatik ravishda anonim shaklda uzatiladigan
+                maʼlumotlar.
+                <br />
+                1.4. Maʼmuriyat foydalanuvchidan Sayt yordamida to‘plangan
+                anonim maʼlumotlarning tarkibidagi talablarni belgilash huquqiga
+                ega.
+                <br />
+                1.5. Agar biror-bir maʼlumot majburiy deb belgilanmagan bo‘lsa,
+                uni taqdim etish yoki oshkor qilish Foydalanuvchi ixtiyorida
+                bo‘ladi. Shu bilan birga, siz cheksiz doiralarning bunday
+                maʼlumotlarga kirishiga rozilik bildirasiz. Ko‘rsatilgan
+                maʼlumotlar taqdim qilingan yoki boshqa shaklda ochilgan paytdan
+                boshlab hammaga ochiq bo‘ladi.
+                <br /> 1.6. Maʼmuriyat taqdim etilgan maʼlumotlarning
+                to‘g‘riligini tekshirmaydi va Foydalanuvchi ushbu Siyosatga
+                muvofiq ularni qayta ishlashga zarur roziligini oladi.
+                Foydalanuvchi vijdonan harakat qiladi va ehtiyotkorlik bilan
+                ishlaydi hamda bunday maʼlumotlarni yangilab turish, undan
+                foydalanish uchun barcha zarur roziliklarni olishga harakat
+                qiladi. Siz Saytda uchinchi tomon dasturlaridan foydalanish
+                imkoniyatini tan olasiz va qabul qilasiz, buning natijasida
+                bunday shaxslar 1.3-bandda ko‘rsatilgan maʼlumotlarni sizga
+                nomaʼlum shaklda olishlari va uzatishlari mumkin.
+                <br /> 1.7. Uchinchi tomon dasturlari yordamida anonim
+                maʼlumotni yig‘ish tarkibi va shartlari bevosita ularning
+                egalari tomonidan belgilanadi hamda quyidagilarni o‘z ichiga
+                olishi mumkin: - Brauzer maʼlumotlari (turi, versiyasi, “cookie”
+                fayllari); - Qurilma maʼlumotlari va uning holati; - Operatsion
+                tizim maʼlumotlari (turi, versiyasi, ekran o‘lchamlari); -
+                So‘rov maʼlumotlari (vaqt, o‘tish manbai, IP manzili).
+                <br /> 1.8. Maʼmuriyat uchinchi tomon tomonidan
+                foydalanuvchining Anonim maʼlumotlarini ishlatish tartibi uchun
+                javobgar bo‘lmaydi.
+              </p>
+              <p>
+                <span className="font-semibold text-lg">
+                  2. Maʼlumotlarni qayta ishlash maqsadlari
+                </span>{" "}
+                <br /> 2.1. Maʼmuriyat maʼlumotlardan quyidagi maqsadlarda
+                foydalanadi: <br />
+                2.1.1. Kirish so‘rovlari va foydalanuvchi bilan aloqani qayta
+                ishlash;
+                <br /> 2.1.2. Axborot xizmatlari, shu jumladan reklama
+                materiallarini tarqatish;
+                <br /> 2.1.3. Marketing, statistik va boshqa tadqiqotlarini
+                o‘tkazish;
+                <br /> 2.1.4. Saytdagi reklama materiallarini yo‘naltirish.
+              </p>
+              <p>
+                <span className="font-semibold text-lg">
+                  3. Maʼlumotlarni himoya qilish talablari
+                </span>{" "}
+                <br />
+                3.1. Maʼmuriyat maʼlumotlarni saqlaydi va ularni ichki
+                qoidalarga muvofiq ruxsatsiz kirish va tarqatishdan himoya
+                qiladi. <br /> 3.2. Olingan maʼlumotlarga nisbatan maxfiylik
+                saqlanadi, agar ular Foydalanuvchi tomonidan ochiq bo‘lsa,
+                shuningdek saytda ishlatiladigan uchinchi tomon texnologiyalari
+                va dasturlari yoki foydalanuvchi tomonidan ishlatiladigan
+                dasturiy taʼminot ushbu shaxslar yoki boshqa foydalanuvchilar va
+                internet foydalanuvchilari bilan ochiq almanishuvni taʼminlagan
+                hollar bundan mustasno. <br /> 3.3. Ish sifatini yaxshilash
+                uchun 1 (bir) yil davomida Saytdan foydalanish doirasida
+                Foydalanuvchi tomonidan amalga oshirilgan harakatlar
+                to‘g‘risidagi jurnal fayllarini saqlash huquqiga ega.
+              </p>
+              <p>
+                <span className="font-semibold text-lg">
+                  4. Maʼlumot uzatish
+                </span>
+                <br /> 4.1. Maʼmuriyat quyidagi hollarda maʼlumotlarni uchinchi
+                shaxslarga uzatish huquqiga ega: - Foydalanuvchi bunday
+                harakatlarga, shu jumladan foydalanuvchi maʼlum bir maʼlumotni
+                berishni cheklamaydigan dasuturiy taʼminot sozlamalarini
+                ishlatgan holatlarda roziligini bildiradi; - Maʼlumotni qayta
+                ishlash, maqsadga muvofiq uzatish talab etiladi: - Foydalanuvchi
+                tomonidan sodir etilgan qonunbuzarlik sababli, Maʼmuriyat
+                huquqlari qonuniy himoya qilinadi.
+              </p>
+              <p>
+                <span className="font-semibold text-lg">
+                  5. Maxfiylik siyosatining o‘zgarishi
+                </span>
+                <br />
+                5.1. Ushbu Siyosat Maʼmuriyat tomonidan bir tomonlama tartibda
+                foydalanuvchi tomonidan ogohlantirilmasdan o‘zgartilirishi yoki
+                bekor qilinishi mumkin. Agar Siyosatning yangi variantida
+                boshqacha qoidalar nazarda tutilgan bo‘lmasa, yangi siyosat
+                Saytga joylashtirilgan paytdan boshlab kuchga kiradi. <br />{" "}
+                5.2. Siyosatning joriy talqini saytga joylashtirilgan.
+              </p>
+            </div>
           </div>
-        </section>
+        </div>
       </Layout>
     </>
   );

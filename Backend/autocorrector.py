2ba8be365c57555b1dbfc107ff37031f0e2635db
Giyos1 <giyosoripov4@gmail.com>
2022-10-21 17:49:18 +0500

    add hunspell

--- Backend/autocorrector.py
+++ Backend/autocorrector.py
@@ -1,15 +1,19 @@
 import hunspell
+from texttools.settings import BASE_DIR
+from os.path import join as join_path
 
-spellchecker = hunspell.HunSpell('/home/samandar/samandar/airiuz/tilim.uz/Backend/uz-hunspell/uz-lat.dic',
-                                 '/home/samandar/samandar/airiuz/tilim.uz/Backend/uz-hunspell/uz-lat.aff')
+spellchecker = hunspell.HunSpell(join_path(BASE_DIR, "uz-hunspell/uz-lat.dic"),
+                                 join_path(BASE_DIR, "uz-hunspell/uz-lat.aff"))
 
-spellcheckercyr = hunspell.HunSpell('/home/samandar/samandar/airiuz/tilim.uz/Backend/uz-hunspell/uz-cyr.dic',
-                                 '/home/samandar/samandar/airiuz/tilim.uz/Backend/uz-hunspell/uz-cyr.aff')
+spellcheckercyr = hunspell.HunSpell(join_path(BASE_DIR, 'uz-hunspell/uz-cyr.dic'),
+                                    join_path(BASE_DIR, 'uz-hunspell/uz-cyr.aff'))
+
 
 def check(suz, cyr=False):
     if not cyr:
         return spellchecker.spell(suz)
     return spellcheckercyr.spell(suz)
 
+
 def suggestions(suz):
-	return spellchecker.suggest(suz)
+    return spellchecker.suggest(suz)
--- Backend/translit/storage.py
+++ Backend/translit/storage.py
@@ -1,16 +1,15 @@
 import os
 from django.core.files.storage import FileSystemStorage
 
+
 class MyStorage(FileSystemStorage):
 
     def get_available_name(self, name, max_length=None):
         if self.exists(name):
             dir_name, file_name = os.path.split(name)
-            file_root, file_ext = os.path.splitext(file_name)            
+            file_root, file_ext = os.path.splitext(file_name)
 
             my_chars = '1'  # The characters you want to append
 
             name = os.path.join(dir_name, '{}_{}{}'.format(file_root, my_chars, file_ext))
         return name
-
-

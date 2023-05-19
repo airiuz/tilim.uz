d54c2017dfca698b5bf129c898880aa442369563
khabib <habib.sharopov2001@mail.ru>
2023-05-19 16:06:26 +0500

    message

---
+++ frontend/src/components/Loader/index.tsx
@@ -0,0 +1,15 @@
+export const Loader = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      loading...
+    </div>
+  );
+};

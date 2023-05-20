af8de83bf1364a2938048cbbcae09fc75d65df78
samandar <samandar200527@gmail.com>
2023-05-20 09:00:55 +0500

    percent corrected

--- Backend/translit/type_fast.py
+++ Backend/translit/type_fast.py
@@ -2,7 +2,7 @@ def find_difference_text(m_text, input_text):
     out_words = [x for x in m_text.split(' ')]
     in_words = [x for x in input_text.split(' ')]
     difference = [in_words[n] for n in list(range(len(in_words))) if out_words[n] == in_words[n]]  
-    percent = (len(out_words) * len(difference))//100
+    percent = (len(difference) * 100)//len(out_words)
     return {"true_answers":difference, "percent":percent}
 
 

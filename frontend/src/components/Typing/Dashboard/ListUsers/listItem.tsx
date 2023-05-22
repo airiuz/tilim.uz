3f051b967913cd0cc3753450d5d0c43f99da6894
khabib <habib.sharopov2001@mail.ru>
2023-05-22 13:41:43 +0500

    message

--- frontend/src/components/Typing/Dashboard/ListUsers/listItem.tsx
+++ frontend/src/components/Typing/Dashboard/ListUsers/listItem.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import styles from "./index.module.css";
+import { IUser } from "@/src/constants";
 
-const ListItem = () => {
+const ListItem = ({ user }: { user: IUser }) => {
+  console.log(user);
   return (
     <div className={styles.list__user__item}>
-      <div className={styles.number}>1</div>
+      <div className={styles.number}>{user.place}</div>
       <div className={styles.user__information}>
-        <div className={styles.name}>Eshonov Fakhriyor</div>
+        <div className={styles.name}>{user.name}</div>
         <div className={styles.result}>
-          <span>1534 so’z/daq</span>
-          <span>1534 belgi/daq</span>
-          <span>100% aniqlik</span>
+          <span>{user.true_answers} so’z/daq</span>
+          <span>{user.chars} belgi/daq</span>
+          <span>{user.percent}% aniqlik</span>
         </div>
       </div>
     </div>

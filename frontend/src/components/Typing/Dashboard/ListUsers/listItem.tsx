168d303005770ace405c2c8b2206508ca15ddee3
Habib <habib@MacBook-Pro-Habib.local>
2025-05-05 14:39:07 +0500

    commit

--- frontend/src/components/Typing/Dashboard/ListUsers/listItem.tsx
+++ frontend/src/components/Typing/Dashboard/ListUsers/listItem.tsx
@@ -7,7 +7,12 @@ const ListItem = ({ user, place }: { user: IUser; place: number }) => {
     <div className={styles.list__user__item}>
       <div className={styles.number}>{place + 1}</div>
       <div className={styles.user__information}>
-        <div className={styles.name}>{user.fullName}</div>
+        <div
+          className={styles.name}
+          style={{ textTransform: place === 0 ? "uppercase" : "none" }}
+        >
+          {user.fullName}
+        </div>
         <div className={styles.result}>
           <span>{user.wpm} so’z/daq</span>
           <span>{user.accuracy}% aniqlik</span>
